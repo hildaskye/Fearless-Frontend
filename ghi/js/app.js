939605d04cf49edaffe025fce184448435f33d5b
Dylan <Dylanwinn09@gmail.com>
2023-04-04 19:41:37 -0600

    Got cards working and showing 3 per column

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,45 @@
-window.addEventListener('DOMContentLoaded', () => {
+function createCard(name, description, pictureUrl) {
+  return `
+    <div class="card">
+      <img src="${pictureUrl}" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">${description}</p>
+      </div>
+    </div>
+  `;
+}
 
+window.addEventListener('DOMContentLoaded', async () => {
+  const url = 'http://localhost:8000/api/conferences/';
+  const columns = document.querySelectorAll('.col-md-4');
 
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      // Figure out what to do when the response is bad
+    } else {
+      const data = await response.json();
+
+      for (let i = 0; i < data.conferences.length; i++) {
+        const conference = data.conferences[i];
+        const detailUrl = `http://localhost:8000${conference.href}`;
+        const detailResponse = await fetch(detailUrl);
+
+        if (detailResponse.ok) {
+          const details = await detailResponse.json();
+          const title = details.conference.name;
+          const description = details.conference.description;
+          const pictureUrl = details.conference.location.picture_url;
+          const html = createCard(title, description, pictureUrl);
+          const column = columns[i % columns.length];
+          column.innerHTML += html;
+          console.log(html);
+        }
+      }
+    }
+  } catch (e) {
+    // Figure out what to do if an error is raised
+  }
 });
