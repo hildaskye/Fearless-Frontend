78e399c319f5480c88af2c24db612095624e765b
Dylan <Dylanwinn09@gmail.com>
2023-04-11 17:09:58 -0600

    Catchall Commit

--- attendees_microservice/attendees_bc/settings.py
+++ attendees_microservice/attendees_bc/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django_crontab",
     "attendees.apps.AttendeesConfig",
     "django.contrib.admin",
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -55,6 +57,11 @@ MIDDLEWARE = [
 
 CRONJOBS = [
     ("* * * * *", "attendees.poll.get_conferences"),
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:3001",
 ]
 
 ROOT_URLCONF = "attendees_bc.urls"
